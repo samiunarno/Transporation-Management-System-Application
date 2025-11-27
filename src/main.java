07b67114964d7b9021bc651ed729a1a56334a4da
samiunarno <samiunarno.iub@gmail.com>
2025-11-27 22:49:49 +0800

    Git Init Success

---
+++ src/main.java
@@ -0,0 +1,8 @@
+import services.*;
+import utils.FileStorage;
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
